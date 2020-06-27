f9034d5ae9463d52ffff16a1b45043f6b8921bef
Serg <a7mda7s@mail.ru>
2020-06-27 18:12:03 +0300

    20200627 figure selection is working

--- happy-figures/happy-figures-s/src/components/Game.js
+++ happy-figures/happy-figures-s/src/components/Game.js
@@ -3,37 +3,33 @@ import './Game.css';
 import {randomArrayValue} from './utils';
 
 const figuresBorderRadius = () => ['0', '50%'];
-const figuresColor = () => ['#E91E63', '#FF9800', '#FFEB3B'];
-const figureEtalon = () => [
-  {id: 1, backgroundColor: randomArrayValue(figuresColor()), borderRadius: randomArrayValue(figuresBorderRadius()), border: 'none'},
-  {id: 2, backgroundColor: randomArrayValue(figuresColor()), borderRadius: randomArrayValue(figuresBorderRadius()), border: 'none'},
-  {id: 3, backgroundColor: randomArrayValue(figuresColor()), borderRadius: randomArrayValue(figuresBorderRadius()), border: 'none'},
+const figuresColor = () => ['#E91E63', '#FFEB3B', '#FF5722'];
+const figureEtalon = [
+  {id: '0', backgroundColor: randomArrayValue(figuresColor()), borderRadius: randomArrayValue(figuresBorderRadius()), border: 'none'},
+  {id: '1', backgroundColor: randomArrayValue(figuresColor()), borderRadius: randomArrayValue(figuresBorderRadius()), border: 'none'},
+  {id: '2', backgroundColor: randomArrayValue(figuresColor()), borderRadius: randomArrayValue(figuresBorderRadius()), border: 'none'},
 ];
-
-//TODO : если заменить id на data-id, то можно обойтись без этой функции
-//и использовать elem в стилизации
 
 //TODO : использовать компонент ListFigure для обоих случаев
 
-//убирает из объекта elem поле id, т.к. elem является объектом стилей
-//и строит новый массив JSX-елементов
-const buildListFigureEtalon = figureEtalon().map(elem => {
-  const id = elem.id;
-  // console.log(elem);
-  delete elem.id;
-  // console.log(elem);
-  return (
-    <span className='figure figure--etalon' key={id} style={elem}></span>
-  )
-});
+//убирает из объекта elem поле id, т.к. elem является объектом стилей и строит новый массив JSX-елементов
+// const buildListFigureEtalon = figureEtalon().map(elem => {
+//   const id = elem.id;
+//   // console.log(elem);
+//   delete elem.id;
+//   // console.log(elem);
+//   return (
+//     <span className='figure figure--etalon' key={id} style={elem}></span>
+//   )
+// });
 
 class Game extends Component {
   state = {
     count: 0,
     figureSelection: [
-      {id: 1, backgroundColor: 'transparent', borderRadius: '0', border: '1px solid #673ab7', cliked: false},
-      {id: 2, backgroundColor: 'transparent', borderRadius: '0', border: '1px solid #673ab7', cliked: false},
-      {id: 3, backgroundColor: 'transparent', borderRadius: '0', border: '1px solid #673ab7', cliked: false},
+      {id: '0', backgroundColor: 'transparent', borderRadius: '0', border: '1px solid #673ab7', cliked: false},
+      {id: '1', backgroundColor: 'transparent', borderRadius: '0', border: '1px solid #673ab7', cliked: false},
+      {id: '2', backgroundColor: 'transparent', borderRadius: '0', border: '1px solid #673ab7', cliked: false},
     ],
     // figureEtalon: [
     //   {backgroundColor: 'transparent', borderRadius: randomArrayValue(figuresBorderRadius), border: '1px solid #673ab7'},
@@ -41,6 +37,52 @@ class Game extends Component {
     //   {backgroundColor: 'transparent', borderRadius: randomArrayValue(figuresBorderRadius), border: '1px solid #673ab7'},
     // ],
 
+  }
+
+  figureSelectionHandleClick = (e) => {
+    const id = e.target.id;
+    const { figureSelection } = this.state;
+
+    console.log(id);
+    console.log(figureSelection[id]);
+
+    // const idx = arr.findIndex((el) => el.id === id);
+    // const oldItem = arr[idx];
+    // const newItem = {...oldItem, [propName] : !oldItem[propName]};
+    const newFigure = {
+      id, 
+      backgroundColor: 'red', 
+      borderRadius: '0', 
+      border: 'none', 
+      cliked: true
+    };
+
+    // return [
+    //   ...arr.slice(0, idx), 
+    //   newItem, 
+    //   ...arr.slice(idx + 1)
+    // ];
+    console.log('***********figureSelection*******************');
+    console.log(figureSelection);
+    console.log('***********figureSelection*******************');
+    console.log(figureSelection.slice(0, id));
+    console.log(newFigure);
+    console.log(figureSelection.slice(id+1));
+
+    // let newFigureSelection = [
+    //   ...figureSelection.slice(0, id), 
+    //   newFigure, 
+    //   ...figureSelection.slice(id+1)
+    // ];
+  
+    let newFigureSelection = [...figureSelection];
+    newFigureSelection[id] = newFigure;
+  
+console.log(newFigureSelection);
+
+    this.setState({
+      figureSelection: newFigureSelection
+    });
   }
 
   render() {
@@ -55,13 +97,45 @@ class Game extends Component {
         </div>
 
         <div className='Game__figures'>
-          <span className='figure figure--selection' style={figureSelection[0]}></span>
-          <span className='figure figure--selection' style={figureSelection[1]}></span>
-          <span className='figure figure--selection' style={figureSelection[2]}></span>
+          {
+            figureSelection.map(elem => {
+              //убирает из объекта elem поле id и поле clicked, т.к. elem является объектом стилей и строит новый массив JSX-елементов
+              const id = elem.id;
+              // console.log(elem);
+              // delete elem.id;
+              // delete elem.cliked;
+              // console.log(elem);
+              return (
+                <span 
+                  className='figure figure--selection' 
+                  key={id} 
+                  id={id}
+                  style={elem}
+                  onClick={(e) => this.figureSelectionHandleClick(e)}
+                ></span>
+              )
+            })
+          }
         </div>
 
         <div className='Game__figures'>
-          {buildListFigureEtalon}
+          {
+            figureEtalon.map(elem => {
+              //убирает из объекта elem поле id, т.к. elem является объектом стилей и строит новый массив JSX-елементов
+              const id = elem.id;
+              // console.log(elem);
+              // delete elem.id;
+              // console.log(elem);
+              return (
+                <span 
+                  className='figure figure--etalon' 
+                  key={id}
+                  id={id} 
+                  style={elem}
+                ></span>
+              )
+            })
+          }
         </div>
 
       </div>
@@ -72,8 +146,18 @@ class Game extends Component {
 export default Game;
 
 /***
- * const listItems = numbers.map((number) =>
-    // Правильно! Ключ нужно определять внутри массива:
-    <ListItem key={number.toString()} value={number} />
-  );
+ *
+ * 
+TODO : сделать генерацию фигур при кликах
+
+TODO : отследить момент, когда все фигуры будут сгенерированы и надо начинать проверку
+
+TODO : где делать проверки? в componentDidMount?
+
+TODO : элементы figureSelection не должны перегенерироваться при повторном клике
+т.е. если поле cliked = true
+
+
  */
+
+
